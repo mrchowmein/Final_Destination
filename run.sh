d4662e692c7a5f8b224055d549bf407f2f50a3bc
Jason <jasonfchan@gmail.com>
2019-10-13 21:37:38 -0700

    Update run.sh
    removed zips processings to speed up

--- run.sh
+++ run.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 #
 #
-#remove old zipcode files from hdfs
-hdfs dfs -rm -r /zipcode_tables/stationZip.csv
-hdfs dfs -rm -r /zipcode_tables/taxiZoneZips.csv
-
-#run geocoding scripts to create updated zipcode list
-python3 ./TaxiZipcode/TaxiZip.py
-python3 ./CitiBikeZipcode/CitiStationZips.py
-
-#put updated zipcode list to hdfs
-hdfs dfs -put ./TaxiZipcode/taxiZoneZips.csv /zipcode_tables
-hdfs dfs -put ./CitiBikeZipcode/stationZip.csv /zipcode_tables
-
+# to update zips, run manually
 #run spark script to process all datasets
 spark-shell -i ./Processing/Combined_Processing.scala --conf spark.driver.args="$1 $2" --jars /home/ubuntu/postgresql-42.2.8.jar
